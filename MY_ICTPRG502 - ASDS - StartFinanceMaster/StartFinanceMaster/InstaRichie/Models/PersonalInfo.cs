c85ed2bba42ae54baa24795aaf503461b936114a
Kiel Caralipio <kiel_caralipio@yahoo.com>
2021-03-17 15:39:48 +1030

    Added PersonalInfo.cs - kiel

---
+++ MY_ICTPRG502 - ASDS - StartFinanceMaster/StartFinanceMaster/InstaRichie/Models/PersonalInfo.cs
@@ -0,0 +1,20 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    public class PersonalInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int PersonalId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int MobilePhone { get; set; }
+    
+    }
+}
