bebf1765370b6a122d071623599e048cff276f28
Matthew Gerlach <bnyney2k@gmail.com>
2021-03-21 15:30:40 +1030

    Added model and interface repository classes. Added blank XAML files ready for UI.

---
+++ MY_ICTPRG502 - ASDS - StartFinanceMaster/StartFinanceMaster/InstaRichie/Models/ContactDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+//Author: Matt Gerlach
+
+namespace StartFinance.Models
+{
+    public class ContactDetail
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CompanyName { get; set; }
+        public string MobilePhone { get; set; }
+    }
+}
---
+++ MY_ICTPRG502 - ASDS - StartFinanceMaster/StartFinanceMaster/InstaRichie/Models/IContactDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Author: Matt Gerlach
+
+namespace StartFinance.Models
+{
+    public interface IContactDetailRepository
+    {
+        IEnumerable<ContactDetail> GetContactDetails();
+        ContactDetail GetContactDetailById(int contactId);
+        bool InsertCustomer(ContactDetail contactDetail);
+        bool DeleteCustomer(int contactId);
+        bool UpdateCustomer(ContactDetail contactDetail);
+    }
+}
