00b28d5ea0a77ec228c34ed0c73396397dfd21bf
Nancy <lianzi1865@gmail.com>
2021-03-22 05:06:33 +0000

    Add ShoppingList.cs in models

---
+++ MY_ICTPRG502 - ASDS - StartFinanceMaster/StartFinanceMaster/InstaRichie/Models/ShoppingList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    class ShoppingList
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ShoppingItemID {get; set;}
+        public String ShopName {get; set;}
+        public  String NameOfItem { get; set; }
+        public DateTime ShoppingDate { get; set; }
+        public double PriceQuoted { get; set; }
+
+    }
+}
