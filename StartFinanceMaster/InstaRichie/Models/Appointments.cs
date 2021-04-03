10bf1f430c582bd541927a7b4edbd89d7446d532
Zheru Chen <zheru.chen@student.tafesa.edu.au>
2021-04-03 12:16:42 +1030

    Add the appointmentPage in the views (modified from the original assessment file)

---
+++ StartFinanceMaster/InstaRichie/Models/Appointments.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    class Appointments
+    {
+        [PrimaryKey, AutoIncrement]
+        public int AppointmentID { get; set; }
+
+        [Unique]
+        public string EventName { get; set; }
+
+        [NotNull]
+        public string Location { get; set; }
+
+        [NotNull]
+        public string PhoneNo { get; set; }
+
+        [NotNull]
+        public string Date { get; set; }
+
+        [NotNull]
+        public string StarTime { get; set; }
+
+        [NotNull]
+        public string EndDate { get; set; }
+
+        [NotNull]
+        public string Description { get; set; }      
+
+        [NotNull]
+        public string SelectTypeOfService { get; set; }
+
+    }
+}
